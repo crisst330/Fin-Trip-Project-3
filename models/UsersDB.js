90358e35b5b84b1ad5d5405ede8751b0b5c89cdf
crisst <criss.t@northeastern.edu>
2026-07-15 01:33:23 -0400

    Created Users Database Collection & Installed the mongodb module via npm install
    In this commit, I created the required file that creates a MongoDB client connection for our users database collection. Also, I installed the mongodb module for the backend package.json file. Next, I will test with Mongo Compass and run the docker container for the mongodb image to create a mongodb-test server to use in a test case scenario.

---
+++ models/UsersDB.js
@@ -0,0 +1,96 @@
+import { MongoClient, ObjectId } from "mongodb";
+import process from "process";
+
+// Database configuration
+function UsersDB({
+    dbName = "finTrip",
+    collectionName = "users",
+    defaultURI = "mongodb://localhost:27017",
+} = {}) {
+    const me = {};
+    const URI = process.env.MONGODB_URI || defaultURI;
+
+    // Connection function
+    const connect = async () => {
+        console.log("Connecting to MongoDB at", URI);
+
+        const client = new MongoClient(URI);
+        await client.connect();
+
+        const users = client.db(dbName).collection(collectionName);
+
+        // Returns both the collection & the client
+        return { client, users };
+    };
+
+    // 1st Authentication function
+    // Creates a user document resembling: { _id:, name:, email:, passwordHash: }
+    me.createUsers = async ({ name, email, passwordHash }) => {
+        // Opens the MongoDB connection
+        const { client, users } = await connect();
+
+        try {
+            const user = {
+                name, 
+                email,
+                passwordHash,
+            };
+
+            const result = await users.insertOne(user);
+
+            return {
+                ...user,
+                _id: result.insertedId,
+            };
+        } catch (err) {
+            console.error("Error creating user in MongoDB", err);
+            throw err;
+        } finally {
+            await client.close();
+        }
+    };
+
+    // 2nd Authentication function
+    me.findUserByEmail = async (email) => {
+        // Opens the MongoDB connection
+        const { client, users } = await connect();
+
+        try {
+            return await users.findOne({ email });
+        } catch (err) {
+            console.error("Error finding user by email", err);
+            throw err;
+        } finally {
+            await client.close();
+        }
+    };
+
+    // 3rd Authentication function
+    // Passport will use this when restoring logged-in users from their assocated sessions.
+    me.findUserById = async (userId) => {
+        // Opens the MongoDB connection
+        const { client, users } = await connect();
+
+        try {
+            // Prevents MongoDB from throwing an error if an invalid ID reaches this function
+            if (!ObjectId.isValid(userId)) {
+                return null;
+            }
+
+            return await users.findOne({
+                _id: new ObjectId(userId),
+            });
+        } catch (err) {
+            console.error("Error finding user by ID", err);
+            throw err;
+        } finally {
+           await client.close();
+        }
+    };
+
+    return me;
+}
+
+const usersDB = UsersDB();
+
+export default usersDB;
