0ee4dbaca5acdb0c9b953bdf939c7ef1b633b005
crisst <criss.t@northeastern.edu>
2026-07-15 22:37:27 -0400

    Created the route "/api/auth/user' for confirmation of login session cookie authentication
    In this commit, I successfully created the route that handles login session cookie logic, requests, and determines the current user logged in during a session logic via the React frontend.

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,10 +1,13 @@
 import express from "express";
 import bcrypt from "bcrypt"; // Hashes passwords
+import passport from "passport";
+
 import usersDB from "../models/UsersDB.js";
 
 // Groups all authentication endpoints together (api routes)
 const router = express.Router();
 
+// Authentication routes
 router.post("/register", async (req, res) => {
     try {
         // Request body: { "name": "TestName", "email": "test123@example.com", "password": "password123" => hashed}
@@ -46,6 +49,49 @@ router.post("/register", async (req, res) => {
             error: "Unable to register this user.",
         });
     }
+});
+
+router.post("/login", (req, res, next) => {
+    passport.authenticate("local", (error, user, info) => {
+        if (error) {
+            console.error(error);
+
+            return res.status(500).json({
+                error: "Unable to login in at this time."
+            });
+        }
+
+        if (!user) {
+            return res.status(401).json({
+                error: info?.message || "Unable to log in user.",
+            });
+        }
+        req.logIn(user, (loginError) => {
+            if (loginError) {
+                return next(loginError);
+            }
+
+            return res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            });
+        });
+    }) (req, res, next);
+});
+
+router.get("/user", (req, res) => {
+    if (!req.isAuthenticated()) {
+        return res.status(401).json({
+            error: "This user is not authenticated.",
+        });
+    }
+
+    return res.status(200).json({
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email,
+    });
 });
 
 export default router;
