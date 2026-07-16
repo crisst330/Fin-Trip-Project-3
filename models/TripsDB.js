460c4f6d263ed9a81d23f86aac4c1cf34a61e4c6
crisst <criss.t@northeastern.edu>
2026-07-16 04:05:05 -0400

    Finished all trip-level methods (CRUD work) in the TripsDB.js file
    In this commit, I finished my division of work regarding all of the trip db methods as part of the TripsDB.js file.

--- models/TripsDB.js
+++ models/TripsDB.js
@@ -17,6 +17,139 @@ function TripsDB({
     return { client, trips };
   };
 
+  me.getTrips = async (ownerId) => {
+    const { client, trips } = await connect();
+
+    try {
+      return await trips.find({ownerId: new ObjectId(ownerId),
+      })
+      .toArray();
+    } catch (error) {
+      console.error("Error finding trips:", error);
+      throw error;
+    } finally {
+      await client.close();
+    }
+  };
+
+  me.getTripById = async (tripId, ownerId) => {
+    if (!ObjectId.isValid(tripId) || !ObjectId.isValid(ownerId)) {
+      return null;
+    }
+
+    const { client, trips } = await connect();
+
+    try {
+      return await trips.findOne({
+        _id: new ObjectId(tripId),
+        ownerId: new ObjectId(ownerId),
+      });
+    } catch (error) {
+      console.error("Error finding trip by ID:", error);
+      throw error;
+    } finally {
+      await client.close();
+    }
+  };
+
+  // This method stores the following: {_id: ObjectId("..."), ownerId: ObjectId("logged-in-user-id"), name:, etc}
+  // aka, the database record
+  me.createTrip = async ({
+    ownerId,
+    name,
+    destination,
+    startDate,
+    endDate,
+    travelers,
+    budgetCap,
+  }) => {
+    const { client, trips } = await connect();
+
+    try {
+      const trip = {
+        ownerId: new ObjectId(ownerId),
+        name,
+        destination, 
+        startDate,
+        endDate,
+        travelers,
+        budgetCap,
+        items: [],
+      };
+
+      const result = await trips.insertOne(trip);
+
+      return {
+        ...trip,
+        _id: result.insertedId,
+      };
+    } catch (error) { 
+      console.error("Error creating trip:", error);
+      throw error;
+    } finally {
+      await client.close();
+    }
+  };
+
+  me.updateTrip = async (tripId, ownerId, updates) => {
+    if (!ObjectId.isValid(tripId) || !ObjectId.isValid(ownerId)) {
+      return null;
+    }
+
+    const { client, trips } = await connect();
+
+    try {
+      const updatedFields = {
+        name: updates.name,
+        destination: updates.destination,
+        startDate: updates.startDate,
+        endDate: updates.endDate,
+        travels: Number(updates.travelers),
+        budgetCap: Number(updates.budgetCap),
+      };
+
+      return await trips.findOneAndUpdate(
+        {
+         _id: new ObjectId(tripId),
+         ownerId: new ObjectId(ownerId), 
+        },
+        {
+          $set: updatedFields,
+        },
+        {
+          returnDocument: "after",
+        },
+      );
+    } catch (error) {
+      console.error("Error updating trip:", error);
+      throw error;
+    } finally {
+      await client.close();
+    }
+  };
+
+  me.deleteTrip = async (tripId, ownerId) => {
+    if (!ObjectId.isValid(tripId) || !ObjectId.isValid(ownerId)) {
+      return false;
+    }
+
+    const { client, trips } = await connect();
+
+    try {
+      const result = await trips.deleteOne({
+        _id: new ObjectId(tripId),
+        ownerId: new ObjectId(ownerId),
+      });
+
+      return result.deletedCount === 1;
+    } catch (error) {
+      console.error("Error deleting trip:", error);
+      throw error;
+    } finally {
+      await client.close();
+    }
+  };
+
   me.addItem = async (tripId, item) => {
     const { client, trips } = await connect();
     try {
