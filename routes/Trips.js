32a04b9189ea8b061bccbc1603247b065ad99566
crisst <criss.t@northeastern.edu>
2026-07-16 20:52:24 -0400

    Fixed /:id route
    In this commit, I simply fixed this route to test if the error we see in the frontend goes away. I'll add debug lines if this fails.

--- routes/Trips.js
+++ routes/Trips.js
@@ -53,16 +53,27 @@ router.put("/:id", async (req, res) => {
     req.params.id,
     req.body,
   );
+
   try {
     const updatedTrip = await tripsDB.updateTrip(
       req.params.id,
       req.user._id,
       req.body,
     );
-    res.status(200).json(updatedTrip);
+
+    if (!updatedTrip) {
+      return res.status(404).json({
+        error: "Trip not found.",
+      });
+    }
+
+    return res.status(200).json(updatedTrip);
   } catch (error) {
     console.error("Error updating trip:", error);
-    res.status(500).json({ error: "Failed to update trip" });
+
+    return res.status(500).json({
+      error: "Failed to update trip.",
+    });
   }
 });
 
